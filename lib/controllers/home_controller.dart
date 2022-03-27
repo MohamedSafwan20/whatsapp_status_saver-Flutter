304878294a93c728bc368ceba73b5bcddb6c0613
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2022-03-27 20:18:27 +0530

    Added staggered grid view

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,10 +1,20 @@
+import 'dart:math';
+
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   int currentPage = 0;
 
+  List<double> videoSizes = [0.9, 1.1, 2];
+
   void onPageChange(int page) {
     currentPage = page;
     update();
+  }
+
+  double getRandomVideoSize() {
+    Random random = Random();
+
+    return videoSizes[random.nextInt(videoSizes.length)];
   }
 }
--- lib/widgets/status.dart
+++ lib/widgets/status.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:get/get.dart';
+import 'package:whatsapp_status_saver/controllers/home_controller.dart';
 
 class Status extends StatefulWidget {
   const Status({Key? key}) : super(key: key);
@@ -8,8 +11,56 @@ class Status extends StatefulWidget {
 }
 
 class _StatusState extends State<Status> {
+  final homeController = Get.find<HomeController>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: StaggeredGrid.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        children: [
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: homeController.getRandomVideoSize(),
+            child: GestureDetector(
+              onTap: homeController.getRandomVideoSize,
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: homeController.getRandomVideoSize(),
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: homeController.getRandomVideoSize(),
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: homeController.getRandomVideoSize(),
+            child: Container(
+              color: Colors.orange,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: homeController.getRandomVideoSize(),
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
